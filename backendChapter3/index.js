89502d0bf91638dc6ebc3827145f6eecaa641314
Adarsh <protoyoru07@gmail.com>
2025-05-28 19:23:56 +0530

    Just first working draft !!

--- backendChapter3/index.js
+++ backendChapter3/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const { log } = require("console");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,8 +9,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   fs.readdir("./files/", (err, files) => {
-    res.render("index", { files: files });
+    console.log(files);
+    res.render("index", { dirfiles: files });
   })
+})
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", { filename: req.params.filename, filedata: filedata });
+  });
 })
 app.post("/create", (req, res) => {
   fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.details, (err) => {
