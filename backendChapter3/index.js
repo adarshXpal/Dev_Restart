8501064413154dcb2a96d4f5f47422bf686d9b79
Adarsh <protoyoru07@gmail.com>
2025-05-30 00:11:22 +0530

    Its all about chapter 3 !!

--- backendChapter3/index.js
+++ backendChapter3/index.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   fs.readdir("./files/", (err, files) => {
@@ -16,6 +16,14 @@ app.get("/", (req, res) => {
 app.get("/file/:filename", (req, res) => {
   fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
     res.render("show", { filename: req.params.filename, filedata: filedata });
+  });
+})
+app.get("/edit/:filename", (req, res) => {
+  res.render("edit", { filename: req.params.filename });
+})
+app.post("/edit/:filename", (req, res) => {
+  fs.rename(`./files/${req.params.filename}`, `./files/${req.body.new.split(" ").join("")}.txt`, (err) => {
+    res.redirect("/");
   });
 })
 app.post("/create", (req, res) => {
